fec4414607dc42962c5014d921d363b45a54073f
Tanner <152333980+Phlegmlee@users.noreply.github.com>
2026-09-06 20:48:05 -0600

    SlotManager - GetFileInfo

--- addons/Saveable/SlotManager.cs
+++ addons/Saveable/SlotManager.cs
@@ -24,7 +24,35 @@ internal static class SlotManager
 		return FileAccess.FileExists(filePath);
 	}
 
-	// TODO: method to get file info
+	/// <summary>
+	/// Returns the date and time the save was last modified.
+	/// </summary>
+	/// <param name="saveSlot">The save slot to check.</param>
+	public static string GetFileInfo(SaveSlotEnum saveSlot)
+	{
+		var unixTime = FileAccess.GetModifiedTime(GetSaveFile(saveSlot));
+		if (unixTime == 0) return "Error: File could not return info.";
+
+		var localTimeBias = Time.GetTimeZoneFromSystem();
+		unixTime += (ulong)localTimeBias["bias"] * 60;
+
+		return Time.GetDatetimeStringFromUnixTime((long)unixTime, true);
+	}
+
+	/// <summary>
+	/// Returns the date and time the save was last modified.
+	/// </summary>
+	/// <param name="filePath">The file path to check.</param>
+	public static string GetFileInfo(string filePath)
+	{
+		var unixTime = FileAccess.GetModifiedTime(filePath);
+		if (unixTime == 0) return "Error: File could not return info.";
+
+		var localTimeBias = Time.GetTimeZoneFromSystem();
+		unixTime += (ulong)localTimeBias["bias"] * 60;
+
+		return Time.GetDatetimeStringFromUnixTime((long)unixTime, true);
+	}
 
 	/// <summary>
 	/// Returns the file path string for a given slot.
